5c7672b0cb5959ec6b2d9711721714397d650db6
gustavo-atanazio <gus.atanazio@gmail.com>
2024-01-16 14:53:30 -0300

    Estrutura da página

--- src/pages/MyQuizzes/index.tsx
+++ src/pages/MyQuizzes/index.tsx
@@ -1,6 +1,31 @@
+import { useLocation } from 'react-router-dom';
+
+import Grid from 'components/Grid';
+import Card from 'components/Card';
+import { useQuizzesContext } from 'context/Quizzes';
+
 function MyQuizzes() {
+    const { pathname } = useLocation();
+    const { quizzes } = useQuizzesContext();
+
     return (
-        <h1>Meus quizzes</h1>
+        <>
+            <h2 className='text-4xl mb-6'>Meus quizzes</h2>
+
+            {quizzes.length > 0
+                ? (
+                    <Grid>
+                        {quizzes.map(quiz => (
+                            <Card {...quiz} path={pathname} key={quiz.id}/>
+                        ))}
+                    </Grid>
+                )
+
+                : (
+                    <p>Nenhum quiz ainda!</p>
+                )
+            }
+        </>
     );
 }
 
