d28bb5adfe788315b512db70b04f04b6c5a5d55f
gustavo-atanazio <gus.atanazio@gmail.com>
2024-01-19 13:20:25 -0300

    Contexto do Modal

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { QuizzesContextProvider } from 'context/Quizzes/index.tsx';
+import { ModalContextProvider } from 'context/Modal/index.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <QuizzesContextProvider>
-            <App />
+            <ModalContextProvider>
+                <App />
+            </ModalContextProvider>
         </QuizzesContextProvider>
     </React.StrictMode>,
 );
