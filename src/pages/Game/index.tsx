ce108c41af0ba2348ea196a431efd64edda26108
gustavo-atanazio <gus.atanazio@gmail.com>
2024-01-24 12:45:58 -0300

    Usando novo contexto Game

--- src/pages/Game/index.tsx
+++ src/pages/Game/index.tsx
@@ -1,38 +1,90 @@
-import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useGameContext } from 'context/Game';
 
 function Game() {
-    const { quiz } = useGameContext();
-    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const navigate = useNavigate();
+    const { state, dispatch } = useGameContext();
 
-    const title = quiz.questions[currentQuestion].title;
-    const options = quiz.questions[currentQuestion].options;
-    const response = quiz.questions[currentQuestion].response;
+    const {
+        quiz,
+        currentQuestion,
+        hasAnswered,
+        correctAnswers,
+        gameOver
+    } = state;
+
+    const isLastQuestion = currentQuestion + 1 >= quiz.questions.length;
+    let title, options;
+
+    if (currentQuestion < quiz.questions.length) {
+        title = quiz.questions[currentQuestion].title;
+        options = quiz.questions[currentQuestion].options;
+    }
 
     return (
         <section className='flex flex-col gap-6 h-full'>
-            <h2 className='text-4xl mb-6'>{quiz.name}</h2>
+            {gameOver
+                ? (
+                    <>
+                        <h2 className='text-4xl mb-6'>O quiz acabou!</h2>
 
-            <p className='text-2xl'>{title}</p>
+                        <p className='text-2xl text-center mb-6'>
+                            Você acertou {correctAnswers} de {quiz.questions.length} perguntas!
+                        </p>
 
-            <div className='flex flex-wrap gap-4'>
-                {options.map((option, index) => (
-                    <div
-                        className='flex-grow basis-64 bg-slate-800 p-2 text-xl text-center rounded'
-                        key={index}
-                    >
-                        {option}
-                    </div>
-                ))}
-            </div>
+                        <button
+                            className='text-xl w-1/2 bg-gray-700 rounded p-2 self-center outline-0'
+                            onClick={() => navigate('/')}
+                        >
+                            Voltar para a Home
+                        </button>
+                    </>
+                )
 
-            <button
-                className='text-xl w-1/2 bg-gray-700 rounded p-2 self-center'
-                onClick={() => setCurrentQuestion(prevState => prevState += 1)}
-                type='button'
-            >
-                Avançar
-            </button>
+                : (
+                    <>
+                        <div>
+                            <h2 className='text-4xl mb-2'>{quiz.name}</h2>
+                            <p className='text-md'>{`Questão ${currentQuestion + 1} de ${quiz.questions.length}`}</p>
+                        </div>
+
+                        <p className='text-2xl'>{title}</p>
+
+                        <div className='flex flex-wrap gap-4'>
+                            {options?.map((option, index) => (
+                                <div
+                                    className={`
+                                        flex-grow basis-64 
+                                        p-2 text-xl text-center cursor-pointer rounded
+                                        bg-slate-800
+                                        ${hasAnswered && 'pointer-events-none'}
+                                    `}
+                                    onClick={() => dispatch({ type: 'CHECK_ANSWER', answer: option })}
+                                    key={index}
+                                >
+                                    {option}
+                                </div>
+                            ))}
+                        </div>
+
+                        <button
+                            className={
+                                `text-xl w-1/2 
+                                bg-gray-700 ${hasAnswered && 'bg-green-600'}
+                                transition-colors rounded p-2 self-center outline-0`
+                            }
+                            onClick={() => {
+                                if (isLastQuestion) dispatch({ type: 'GAME_OVER' });
+                                else dispatch({ type: 'NEXT_QUESTION' });
+                            }}
+                            disabled={!hasAnswered}
+                            type='button'
+                        >
+                            Avançar
+                        </button>
+                    </>
+                )
+            }
         </section>
     );
 }
